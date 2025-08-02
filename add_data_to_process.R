22b219410b5f5e2e72097b0970a7e1bb4b8ab00c
Matteo Conti <matt.ciao@gmail.com>
2025-08-02 17:12:31 +0200

    add data process logic

--- add_data_to_process.R
+++ add_data_to_process.R
@@ -1,6 +1,15 @@
+# LIBS
+library(dplyr)
+
 # VARIABLES
 data_dir <- "data"
 results_dir <- "results"
+
+# UTILS FUNCTIONS
+get_duplicates <- function(names) {
+  duplicates <- names[duplicated(names) | duplicated(names, fromLast = TRUE)]
+  return(duplicates)
+}
 
 # CHECK FILES
 
@@ -10,6 +19,53 @@ if (!file.exists("processed.template.csv")) stop('processed.template.csv is miss
 
 # 2: check folders exists
 if (!dir.exists(data_dir)) stop(paste0(data_dir, "does not exist"))
-if (!dir.exists(results_dir)) stop(paste0(results_dir, "does not exist"))
+if (!dir.exists(paste0(data_dir, '/Connection'))) stop("Connection does not exist")
+if (!dir.exists(paste0(data_dir, '/Cover'))) stop("Cover does not exist")
+if (!dir.exists(paste0(data_dir, '/Sheets'))) stop("Sheets does not exist")
+
+# 3: process with missing files notes
+processed_data_index = read.csv('processed.template.csv')
+connection_files <- connection_files %>% sort()
+
+for (i in 1: length(connection_files)) {
+  has_sheet <- FALSE
+  has_cover <- FALSE
+  
+  if (file.exists(paste0(data_dir, '/Sheets/', 'SHEET_SHEET_', connection_files[i]))) {
+    has_sheet <- TRUE
+  }
+  if (file.exists(paste0(data_dir, '/Cover/', 'COVER_COVER_', connection_files[i]))) {
+    has_cover <- TRUE
+  }
+  
+  missing_files_data <- ''
+  if (!has_sheet & !has_cover) {
+    missing_files_data <- 'SHEET | COVER'
+  } else if (!has_sheet) {
+    missing_files_data <- 'SHEET'
+  } else if (!has_cover) {
+    missing_files_data <- 'COVER'
+  }
+  
+  
+  processed_data_index <- rbind(processed_data_index, 
+                            data.frame(
+                              name = connection_files[i],
+                              missing_files = missing_files_data,
+                              processed = 'FALSE'
+                            ))
+}
+
+
+write.csv(processed_data_index, paste0(results_dir, 'processed.csv'), row.names = FALSE)
+
+print(get_duplicates(connection_files))
+
+sheet_files <- list.files(paste0(data_dir, '/Sheets'), full.names = FALSE)
+print(get_duplicates(sheet_files))
+
+
+cover_files <- list.files(paste0(data_dir, '/Cover'), full.names = FALSE)
+print(get_duplicates(cover_files))
 
 
